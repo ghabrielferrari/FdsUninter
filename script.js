0bce0eed81bcbc5811ac7110b14198ac98f50754
Gabriel Ferrari <ghcferrari75@gmail.com>
2025-04-21 13:56:14 -0300

    Finished

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+// adiciona um evento de clique ao botao com id 'mensagemButton'
 document.getElementById('mensagemButton').addEventListener('click', function() {
+    // seleciona o elemento com id 'mensagem'
     const mensagem = document.getElementById('mensagem');
+
+    // verifica se a mensagem esta oculta (display igual a 'none')
     if (mensagem.style.display === 'none') {
+        // exibe a mensagem alterando o display para 'block'
         mensagem.style.display = 'block';
     } else {
+        // oculta a mensagem alterando o display para 'none'
         mensagem.style.display = 'none';
     }
 });
